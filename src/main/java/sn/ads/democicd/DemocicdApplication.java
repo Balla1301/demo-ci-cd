15093f304bb93a849f24949a79dfc29e8fb7e544
Balla13013 <ballagningue2@gmail.com>
2024-01-08 10:06:56 +0000

    first commit

---
+++ src/main/java/sn/ads/democicd/DemocicdApplication.java
@@ -0,0 +1,15 @@
+package sn.ads.democicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocicdApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemocicdApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
