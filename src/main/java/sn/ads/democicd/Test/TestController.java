95b5e8cecf5d6a324051ada1bc414d0d15ea30b6
Balla13013 <ballagningue2@gmail.com>
2024-07-25 12:06:39 +0000

    update main

---
+++ src/main/java/sn/ads/democicd/Test/TestController.java
@@ -0,0 +1,14 @@
+package sn.ads.democicd.Test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String testEndpoint() {
+        return "Hello from the test endpoint! Application is running successfully.";
+    }
+}
