296f8a7145b3725f70d93cbf0926ba2c12ff4e19
Balla13013 <ballagningue2@gmail.com>
2024-07-26 15:40:12 +0000

    update main

--- src/main/java/sn/ads/democicd/DemocicdApplication.java
+++ src/main/java/sn/ads/democicd/DemocicdApplication.java
@@ -2,15 +2,20 @@ package sn.ads.democicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemocicdApplication {
+public class DemocicdApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemocicdApplication.class);
+	}
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DemocicdApplication.class, args);
 		System.out.println("Hello world");
 		System.out.println("African people world");
 	}
-
 }
