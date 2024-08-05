499ca830e01d7efa0d25444f47f2195e70cc4089
Balla13013 <ballagningue2@gmail.com>
2024-08-05 10:56:50 +0000

    update main

--- src/main/java/sn/ads/democicd/DemocicdApplication.java
+++ src/main/java/sn/ads/democicd/DemocicdApplication.java
@@ -6,12 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemocicdApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemocicdApplication.class);
-	}
+public class DemocicdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemocicdApplication.class, args);
